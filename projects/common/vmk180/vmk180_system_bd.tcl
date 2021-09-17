183dcbd65e8be4aedcf1785940b03dd6959c3c9d
Laszlo Nagy <laszlo.nagy@analog.com>
2021-09-17 11:37:13 +0100

    projects/common: vmk180: vmk180_es1: vck190: Upgrade to 2021.1

--- projects/common/vmk180/vmk180_system_bd.tcl
+++ projects/common/vmk180/vmk180_system_bd.tcl
@@ -22,72 +22,93 @@ create_bd_port -dir I -from 31 -to 0 gpio2_i
 create_bd_port -dir O -from 31 -to 0 gpio2_o
 create_bd_port -dir O -from 31 -to 0 gpio2_t
 
-# instance: versal_cips
+# instance: versal_cips and NoC
 ad_ip_instance versal_cips sys_cips
+
+apply_bd_automation -rule xilinx.com:bd_rule:cips -config { \
+  board_preset {Yes} \
+  boot_config {Custom} \
+  configure_noc {Add new AXI NoC} \
+  debug_config {JTAG} \
+  design_flow {Full System} \
+  mc_type {DDR} \
+  num_mc {1} \
+  pl_clocks {2} \
+  pl_resets {1} \
+}  [get_bd_cells sys_cips]
+
 set_property -dict [list \
-  CONFIG.PS_BOARD_INTERFACE	cips_fixed_io \
-  CONFIG.PMC_SD1_PERIPHERAL_ENABLE {1} \
-  CONFIG.PMC_SD1_SLOT_TYPE {SD 3.0} \
-  CONFIG.PMC_SD1_PERIPHERAL_IO {PMC_MIO 26 .. 36} \
-  CONFIG.PMC_SD1_GRP_CD_ENABLE {1} \
-  CONFIG.PMC_SD1_GRP_POW_ENABLE {1} \
-  CONFIG.PMC_CRP_PL0_REF_CTRL_FREQMHZ {100} \
-  CONFIG.PS_USE_PMCPL_CLK1 {1} \
-  CONFIG.PMC_CRP_PL1_REF_CTRL_FREQMHZ {350} \
-  CONFIG.PS_GPIO_EMIO_PERIPHERAL_ENABLE {1} \
-  CONFIG.PS_USE_M_AXI_GP0 {1} \
-  CONFIG.PS_M_AXI_GP0_DATA_WIDTH {128} \
-  CONFIG.PS_SPI0_GRP_SS1_ENABLE {1} \
-  CONFIG.PS_SPI0_GRP_SS2_ENABLE {1} \
-  CONFIG.PS_SPI0_PERIPHERAL_ENABLE {1} \
-  CONFIG.PS_SPI0_PERIPHERAL_IO {EMIO} \
-  CONFIG.PS_SPI1_GRP_SS1_ENABLE {1} \
-  CONFIG.PS_SPI1_GRP_SS2_ENABLE {1} \
-  CONFIG.PS_SPI1_PERIPHERAL_ENABLE {1} \
-  CONFIG.PS_SPI1_PERIPHERAL_IO {EMIO} \
+  CONFIG.PS_PMC_CONFIG { \
+    CLOCK_MODE Custom \
+    DDR_MEMORY_MODE {Connectivity to DDR via NOC} \
+    DEBUG_MODE JTAG \
+    DESIGN_MODE 1 \
+    IO_CONFIG_MODE Custom \
+    PCIE_APERTURES_DUAL_ENABLE 0 \
+    PCIE_APERTURES_SINGLE_ENABLE 0 \
+    PMC_CRP_PL0_REF_CTRL_FREQMHZ 100 \
+    PMC_GPIO0_MIO_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 0 .. 25}}} \
+    PMC_GPIO1_MIO_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 26 .. 51}}} \
+    PMC_I2CPMC_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 46 .. 47}}} \
+    PMC_MIO37 {{AUX_IO 0} {DIRECTION out} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA high} {PULL pullup} {SCHMITT 0} {SLEW slow} {USAGE GPIO}} \
+    PMC_OSPI_PERIPHERAL {{ENABLE 0} {IO {PMC_MIO 0 .. 11}} {MODE Single}} \
+    PMC_QSPI_COHERENCY 0 \
+    PMC_QSPI_FBCLK {{ENABLE 1} {IO {PMC_MIO 6}}} \
+    PMC_QSPI_PERIPHERAL_DATA_MODE x4 \
+    PMC_QSPI_PERIPHERAL_ENABLE 1 \
+    PMC_QSPI_PERIPHERAL_MODE {Dual Parallel} \
+    PMC_REF_CLK_FREQMHZ 33.3333 \
+    PMC_SD1 {{CD_ENABLE 1} {CD_IO {PMC_MIO 28}} {POW_ENABLE 1} {POW_IO {PMC_MIO 51}} {RESET_ENABLE 0} {RESET_IO {PMC_MIO 1}} {WP_ENABLE 0} {WP_IO {PMC_MIO 1}}} \
+    PMC_SD1_COHERENCY 0 \
+    PMC_SD1_DATA_TRANSFER_MODE 8Bit \
+    PMC_SD1_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 26 .. 36}}} \
+    PMC_SD1_SLOT_TYPE {SD 3.0} \
+    PMC_USE_PMC_NOC_AXI0 1 \
+    PS_BOARD_INTERFACE ps_pmc_fixed_io \
+    PS_CAN1_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 40 .. 41}}} \
+    PS_ENET0_MDIO {{ENABLE 1} {IO {PS_MIO 24 .. 25}}} \
+    PS_ENET0_PERIPHERAL {{ENABLE 1} {IO {PS_MIO 0 .. 11}}} \
+    PS_ENET1_PERIPHERAL {{ENABLE 1} {IO {PS_MIO 12 .. 23}}} \
+    PS_GEN_IPI0_ENABLE 1 \
+    PS_GEN_IPI0_MASTER A72 \
+    PS_GEN_IPI1_ENABLE 1 \
+    PS_GEN_IPI2_ENABLE 1 \
+    PS_GEN_IPI3_ENABLE 1 \
+    PS_GEN_IPI4_ENABLE 1 \
+    PS_GEN_IPI5_ENABLE 1 \
+    PS_GEN_IPI6_ENABLE 1 \
+    PS_GPIO_EMIO_PERIPHERAL_ENABLE 1 \
+    PS_HSDP_EGRESS_TRAFFIC JTAG \
+    PS_HSDP_INGRESS_TRAFFIC JTAG \
+    PS_HSDP_MODE None \
+    PS_I2C1_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 44 .. 45}}} \
+    PS_MIO19 {{AUX_IO 0} {DIRECTION in} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL disable} {SCHMITT 0} {SLEW slow} {USAGE Reserved}} \
+    PS_MIO21 {{AUX_IO 0} {DIRECTION in} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL disable} {SCHMITT 0} {SLEW slow} {USAGE Reserved}} \
+    PS_MIO7 {{AUX_IO 0} {DIRECTION in} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL disable} {SCHMITT 0} {SLEW slow} {USAGE Reserved}} \
+    PS_MIO9 {{AUX_IO 0} {DIRECTION in} {DRIVE_STRENGTH 8mA} {OUTPUT_DATA default} {PULL disable} {SCHMITT 0} {SLEW slow} {USAGE Reserved}} \
+    PS_NUM_FABRIC_RESETS 1 \
+    PS_PCIE1_PERIPHERAL_ENABLE 0 \
+    PS_PCIE2_PERIPHERAL_ENABLE 0 \
+    PS_PCIE_RESET {{ENABLE 1} {IO {PMC_MIO 38 .. 39}}} \
+    PS_PL_CONNECTIVITY_MODE Custom \
+    PS_SPI0 {{GRP_SS0_ENABLE 1} {GRP_SS0_IO EMIO} {GRP_SS1_ENABLE 1} {GRP_SS1_IO EMIO} {GRP_SS2_ENABLE 1} {GRP_SS2_IO EMIO} {PERIPHERAL_ENABLE 1} {PERIPHERAL_IO EMIO}} \
+    PS_SPI1 {{GRP_SS0_ENABLE 1} {GRP_SS0_IO EMIO} {GRP_SS1_ENABLE 1} {GRP_SS1_IO EMIO} {GRP_SS2_ENABLE 1} {GRP_SS2_IO EMIO} {PERIPHERAL_ENABLE 1} {PERIPHERAL_IO EMIO}} \
+    PS_UART0_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 42 .. 43}}} \
+    PS_USB3_PERIPHERAL {{ENABLE 1} {IO {PMC_MIO 13 .. 25}}} \
+    PS_USE_FPD_CCI_NOC 1 \
+    PS_USE_FPD_CCI_NOC0 1 \
+    PS_USE_M_AXI_FPD 1 \
+    PS_USE_NOC_LPD_AXI0 1 \
+    PS_USE_PMCPL_CLK0 1 \
+    PS_USE_PMCPL_CLK1 1  SMON_ALARMS Set_Alarms_On  SMON_ENABLE_TEMP_AVERAGING 0  SMON_TEMP_AVERAGING_SAMPLES 8 \
+    PS_IRQ_USAGE {{CH0 1} {CH1 1} {CH10 1} {CH11 1} {CH12 1} {CH13 1} {CH14 1} {CH15 1} {CH2 1} {CH3 1} {CH4 1} {CH5 1} {CH6 1} {CH7 1} {CH8 1} {CH9 1}} \
+  } \
+  CONFIG.PS_PMC_CONFIG_APPLIED {1} \
+  CONFIG.CLOCK_MODE {Custom} \
+  CONFIG.IO_CONFIG_MODE {Custom} \
+  CONFIG.PS_PL_CONNECTIVITY_MODE {Custom} \
 ] [get_bd_cells sys_cips]
 
-apply_bd_automation -rule xilinx.com:bd_rule:versal_cips -config { configure_noc {Add new AXI NoC} num_ddr {1} pcie0_lane_width {None} pcie0_mode {None} pcie0_port_type {Endpoint Device} pcie1_lane_width {None} pcie1_mode {None} pcie1_port_type {Endpoint Device} pl_clocks {1} pl_resets {1}}  [get_bd_cells sys_cips]
-
-# NOC
-set_property -dict [list \
-  CONFIG.MC_BOARD_INTRF_EN {true} \
-  CONFIG.MC0_CONFIG_NUM {config17} \
-  CONFIG.MC1_CONFIG_NUM {config17} \
-  CONFIG.MC2_CONFIG_NUM {config17} \
-  CONFIG.MC3_CONFIG_NUM {config17} \
-  CONFIG.CH0_DDR4_0_BOARD_INTERFACE {ddr4_dimm1} \
-  CONFIG.MC_INPUT_FREQUENCY0 {200.000} \
-  CONFIG.MC_INPUTCLK0_PERIOD {5000} \
-  CONFIG.MC_MEMORY_DEVICETYPE {UDIMMs} \
-  CONFIG.MC_MEMORY_SPEEDGRADE {DDR4-3200AA(22-22-22)} \
-  CONFIG.MC_TRCD {13750} \
-  CONFIG.MC_TRP {13750} \
-  CONFIG.MC_DDR4_2T {Disable} \
-  CONFIG.MC_CASLATENCY {22} \
-  CONFIG.MC_TRC {45750} \
-  CONFIG.MC_TRPMIN {13750} \
-  CONFIG.MC_CONFIG_NUM {config17} \
-  CONFIG.MC_F1_TRCD {13750} \
-  CONFIG.MC_F1_TRCDMIN {13750} \
-  CONFIG.MC_F1_LPDDR4_MR1 {0x000} \
-  CONFIG.MC_F1_LPDDR4_MR2 {0x000} \
-  CONFIG.MC_F1_LPDDR4_MR3 {0x000} \
-  CONFIG.MC_F1_LPDDR4_MR11 {0x000} \
-  CONFIG.MC_F1_LPDDR4_MR13 {0x000} \
-  CONFIG.MC_F1_LPDDR4_MR22 {0x000} \
-  CONFIG.sys_clk0_BOARD_INTERFACE {ddr4_dimm1_sma_clk} \
-] [get_bd_cells axi_noc_0]
-
-set_property CONFIG.FREQ_HZ 200000000 [get_bd_intf_ports /sys_clk0_0]
-
-set_property -dict [list CONFIG.ASSOCIATED_BUSIF {S01_AXI}] [get_bd_pins /axi_noc_0/aclk1]
-set_property -dict [list CONFIG.ASSOCIATED_BUSIF {S02_AXI}] [get_bd_pins /axi_noc_0/aclk2]
-set_property -dict [list CONFIG.ASSOCIATED_BUSIF {S03_AXI}] [get_bd_pins /axi_noc_0/aclk3]
-set_property -dict [list CONFIG.ASSOCIATED_BUSIF {S04_AXI}] [get_bd_pins /axi_noc_0/aclk4]
-set_property -dict [list CONFIG.ASSOCIATED_BUSIF {S05_AXI}] [get_bd_pins /axi_noc_0/aclk5]
-
-# processor system reset instances for all the three system clocks
 
 ad_ip_instance proc_sys_reset sys_rstgen
 ad_ip_parameter sys_rstgen CONFIG.C_EXT_RST_WIDTH 1
@@ -110,15 +131,9 @@ ad_connect  sys_350m_reset sys_350m_rstgen/peripheral_reset
 ad_connect  sys_350m_resetn sys_350m_rstgen/peripheral_aresetn
 
 # gpio
-ad_ip_instance util_vector_logic gpio_t_inverter [list \
-  C_OPERATION {not} \
-  C_SIZE {32} \
-]
-ad_connect  sys_cips/lpd_gpio_tn  gpio_t_inverter/Op1
-
 ad_connect gpio0_i sys_cips/lpd_gpio_i
 ad_connect gpio0_o sys_cips/lpd_gpio_o
-ad_connect gpio0_t gpio_t_inverter/Res
+ad_connect gpio0_t sys_cips/lpd_gpio_t
 
 # gpio extension witn 64 IOs
 ad_ip_instance axi_gpio axi_gpio
@@ -150,21 +165,21 @@ set sys_dma_resetn        [get_bd_nets sys_350m_resetn]
 
 # spi
 
-ad_connect  sys_cips/spi0_sclk_o spi0_sclk
-ad_connect  sys_cips/spi0_sclk_i GND
-ad_connect  sys_cips/spi0_m_o spi0_mosi
-ad_connect  sys_cips/spi0_m_i spi0_miso
-ad_connect  sys_cips/spi0_s_i GND
-ad_connect  sys_cips/spi0_ss_o_n spi0_csn
-ad_connect  sys_cips/spi0_ss_i_n VCC
+ad_connect  sys_cips/spi0_sck_o spi0_sclk
+ad_connect  sys_cips/spi0_sck_i GND
+ad_connect  sys_cips/spi0_io0_o spi0_mosi
+ad_connect  sys_cips/spi0_io0_i spi0_miso
+ad_connect  sys_cips/spi0_io1_i GND
+ad_connect  sys_cips/spi0_ss_o spi0_csn
+ad_connect  sys_cips/spi0_ss_i VCC
 
-ad_connect  sys_cips/spi1_sclk_o spi1_sclk
-ad_connect  sys_cips/spi1_sclk_i GND
-ad_connect  sys_cips/spi1_m_o spi1_mosi
-ad_connect  sys_cips/spi1_m_i spi1_miso
-ad_connect  sys_cips/spi1_s_i GND
-ad_connect  sys_cips/spi1_ss_o_n spi1_csn
-ad_connect  sys_cips/spi1_ss_i_n VCC
+ad_connect  sys_cips/spi1_sck_o spi1_sclk
+ad_connect  sys_cips/spi1_sck_i GND
+ad_connect  sys_cips/spi1_io0_o spi1_mosi
+ad_connect  sys_cips/spi1_io0_i spi1_miso
+ad_connect  sys_cips/spi1_io1_i GND
+ad_connect  sys_cips/spi1_ss_o spi1_csn
+ad_connect  sys_cips/spi1_ss_i VCC
 
 # system id
 
